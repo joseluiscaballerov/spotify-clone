e932025aded93af22249c330305066247546a02a
unknown <joseluiscaballerow@gmail.com>
2024-10-16 13:39:59 +0200

    Actualizando Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless"; // Cambia esto si es necesario
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel() // Asegúrate de que esto sea correcto
 });
